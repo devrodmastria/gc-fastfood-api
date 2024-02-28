781c42c5794161a4995dc000eddcf72e6833e998
Rod Mastria <rodmastria@gmail.com>
2024-02-28 15:17:15 -0500

    first commit

---
+++ Controllers/DrinksController.cs
@@ -0,0 +1,69 @@
+﻿using FastFoodAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastFoodAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DrinksController : ControllerBase
+    {
+
+        private FastFoodTacoDbContext _dbCtx = new FastFoodTacoDbContext();
+
+        [HttpGet()]
+        public IActionResult GetAllDrinks(string? SortByCost = null)
+        {
+
+            List<Drink> result = _dbCtx.Drinks.ToList();
+            if (SortByCost.ToLower().Contains("asc"))
+            {
+                result = result.OrderBy(d => d.Cost).ToList();
+            } 
+            else if (SortByCost.ToLower().Contains("des"))
+            {
+                result = result.OrderByDescending(d => d.Cost).ToList();
+            }
+                
+            return Ok(result);
+        }
+
+        [HttpGet("GetDrinkById/{id}")]
+        public IActionResult GetDrinkById(int id)
+        {
+            Drink result = _dbCtx.Drinks.FirstOrDefault(t => t.Id == id);
+
+            if (result == null)
+            {
+                return NotFound("Drink not found");
+            }
+            return Ok(result);
+        }
+
+        [HttpPost()]
+        public IActionResult AddDrink([FromBody] Drink newDrink)
+        {
+            if (GetDrinkById(newDrink.Id) == null)
+            {
+                _dbCtx.Drinks.Add(newDrink);
+                _dbCtx.SaveChanges();
+                return Created($"/api/Drinks/{newDrink.Id}", newDrink);
+            }
+            else
+            {
+                return Ok("Drink already created.");
+            }
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateDrink([FromBody] Drink fancierDrink, int id)
+        {
+            if (id != fancierDrink.Id) { return BadRequest("Please enter valid id"); }
+            if (!_dbCtx.Drinks.Any(d => d.Id == id)) { return NotFound("Drink not found"); }
+            _dbCtx.Drinks.Update(fancierDrink);
+            _dbCtx.SaveChanges();
+            return NoContent();
+        }
+
+    }
+}
---
+++ Controllers/TacosController.cs
@@ -0,0 +1,60 @@
+﻿using FastFoodAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastFoodAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TacosController : ControllerBase
+    {
+
+        private FastFoodTacoDbContext _dbCtx = new FastFoodTacoDbContext();
+
+        [HttpGet()]
+        public IActionResult GetAllTacos(bool? softShell = null)
+        {
+
+            List<Taco> result = _dbCtx.Tacos.ToList();
+            if (softShell != null)
+            {
+                result = result.Where(t => t.SoftShell == softShell).ToList();
+            }
+            return Ok(result);
+        }
+
+        [HttpGet("GetTacoById/{id}")]
+        public IActionResult GetTacoById(int id)
+        {
+            Taco result = _dbCtx.Tacos.FirstOrDefault(t => t.Id == id);
+
+            if (result == null)
+            {
+                return NotFound("Taco not found");
+            }
+            return Ok(result);
+        }
+
+        [HttpPost()]
+        public IActionResult AddTaco([FromBody] Taco newTaco)
+        {
+            _dbCtx.Tacos.Add(newTaco);
+            _dbCtx.SaveChanges();
+            return Created($"/api/Tacos/{newTaco.Id}", newTaco);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTaco(int id)
+        {
+            Taco taco  = _dbCtx.Tacos.Find(id);
+            if (taco == null)
+            {
+                return NotFound();
+            }
+            _dbCtx.Tacos.Remove(taco);
+            _dbCtx.SaveChanges();
+            return NoContent();
+            //return Ok("Taco deleted successfully") //my personal preference for good UX
+        }
+
+    }
+}
